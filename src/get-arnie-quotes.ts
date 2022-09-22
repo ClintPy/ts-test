f9a60a09a26b0828d2c46a2143c37924d8cc9d73
Clint Maruti <clintmaruti@Clints-MacBook-Pro.local>
2022-09-22 17:12:47 +0300

    Finishes solving ts-test

--- src/get-arnie-quotes.ts
+++ src/get-arnie-quotes.ts
@@ -1,10 +1,19 @@
-import { httpGet } from './mock-http-interface';
+import { httpGet } from "./mock-http-interface";
 
 // TODO define this type properly
-type TResult = any;
+type TResult = { ["Arnie Quote"]?: string; FAILURE?: string };
+type HTTPResult = { status: number; body: string };
 
-export const getArnieQuotes = async (urls : string[]) : Promise<TResult[]> => {
-  // TODO: Implement this function.
-  
-  return [];
+export const getArnieQuotes = async (urls: string[]): Promise<TResult[]> => {
+    // TODO: Implement this function.
+    const httpResult: Promise<Array<HTTPResult>> = Promise.all(urls.map((url) => httpGet(url)));
+    const TResult: TResult[] = [];
+    (await httpResult).forEach((quote) => {
+        if (quote.status === 200) {
+            TResult.push({ ["Arnie Quote"]: JSON.parse(quote.body).message });
+        } else {
+            TResult.push({ FAILURE: JSON.parse(quote.body).message });
+        }
+    });
+    return TResult;
 };
